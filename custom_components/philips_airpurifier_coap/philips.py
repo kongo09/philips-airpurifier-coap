691ed0c57ad72b4e246b6f4581f2e5d28d02f570
liancea <liancea@mehrkoffe.in>
2024-07-08 16:11:39 +0200

    associate icons with fan preset_mode

--- custom_components/philips_airpurifier_coap/philips.py
+++ custom_components/philips_airpurifier_coap/philips.py
@@ -265,6 +265,12 @@ class PhilipsGenericFan(PhilipsEntity, FanEntity):
         self._model = model
         self._name = name
         self._unique_id = None
+        self._translation_key = "philips_generic_fan"
+
+    @property
+    def translation_key(self) -> Optional[str]:
+        """Return the translation ID of the fan."""
+        return self._translation_key
 
     @property
     def unique_id(self) -> Optional[str]:

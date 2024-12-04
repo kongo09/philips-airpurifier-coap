a5222bd8cc365397fc436b0657a87b9bebed6bf0
kongo09 <kongo09@users.noreply.github.com>
2024-12-04 22:53:44 +0100

    add support for AC0850/41

--- custom_components/philips_airpurifier_coap/philips.py
+++ custom_components/philips_airpurifier_coap/philips.py
@@ -584,6 +584,10 @@ class PhilipsAC085031(PhilipsAC085011):
     """AC0850/31."""
 
 
+class PhilipsAC085041(PhilipsAC085011):
+    """AC0850/41."""
+
+
 class PhilipsAC085081(PhilipsAC085011C):
     """AC0850/81."""
 
@@ -1994,6 +1998,7 @@ model_to_class = {
     FanModel.AC0850_20: PhilipsAC085020,
     FanModel.AC0850_20C: PhilipsAC085020C,
     FanModel.AC0850_31: PhilipsAC085031,
+    FanModel.AC0850_41: PhilipsAC085041,
     FanModel.AC0850_81: PhilipsAC085081,
     FanModel.AC0850_85: PhilipsAC085085,
     FanModel.AC0950: PhilipsAC0950,
